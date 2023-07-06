feb413a6f740f3ce9c99e7c55e95ece2f3403105
Yuvashree <yuvashree.m22122003@gmail.com>
2023-07-06 10:48:56 +0530

    Single user page was added

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,8 +1,52 @@
 import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Chart from "../../components/chart/Chart"
+import List from "../../components/table/Table";
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className="single">
+      <Sidebar />
+      <div className="singlecontainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editbutton">Edit </div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj-czWbX4nyHqjUzCSe1iOQyGCAXmTS_B3-sDdFX8&s" alt="image" className="itemimg" />
+              <div className="details">
+                  <h1 className="itemtitle">John Smith</h1>
+                  <div className="detailitem">
+                    <span className="itemkey">Email:</span>
+                    <span className="itemvalue">abc@gmail.com</span>
+                  </div>
+                  <div className="detailitem">
+                    <span className="itemkey">Phone:</span>
+                    <span className="itemvalue">9876543210</span>
+                  </div>
+                  <div className="detailitem">
+                    <span className="itemkey">Address:</span>
+                    <span className="itemvalue">abc street,melbourne</span>
+                  </div>
+                  <div className="detailitem">
+                    <span className="itemkey">Country:</span>
+                    <span className="itemvalue">USA</span>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={ 3 / 1 } title="User Transacation(Last 6 Months)" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">User Transactions</h1>
+          <List />
+        </div>
+      </div>
+    </div>
   )
 }
 
